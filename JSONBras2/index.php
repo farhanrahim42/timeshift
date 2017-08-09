d2063dbdb1c74540620eca4de4766aebbdc8f2ab
farhanrahim42 <farhan.rahim@tm.com.my>
2017-08-09 16:52:01 -0700

    Update index.php

--- JSONBras2/index.php
+++ JSONBras2/index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo file_get_contents('http://itnthackathon.bweas.tm.com.my/api/getBRASInfo');
-        ?>
-    </body>
-</html>
+    <?php
+	$contents = file_get_contents('http://172.30.201.238:8080/api/getBRASInfo?transform=1);
+	?>

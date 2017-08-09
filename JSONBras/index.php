58fc8e59a15e98afb9dc31810a6e7a3ce66dcdf5
farhanrahim42 <farhan.rahim@tm.com.my>
2017-08-09 11:16:37 -0700

    Update index.php

--- JSONBras/index.php
+++ JSONBras/index.php
@@ -1,6 +1,6 @@
 	<?php
-	$STATE=@$_GET['STATE'];
+	$STATE=@$_GET['REGION'];
 	$SERVICE_TYPE=@$_GET['SERVICE_TYPE'];
-	$contents = file_get_contents("http://172.30.201.238:8080/api/getBRASInfo?transform=1&filter[]=STATE,eq,".urlencode($STATE)."&filter[]=SERVICE_TYPE,eq,".urlencode($SERVICE_TYPE));
+	$contents = file_get_contents("http://172.30.201.238:8080/api/getBRASInfo?transform=1&filter[]=REGION,eq,".urlencode($REGION)."&filter[]=SERVICE_TYPE,eq,".urlencode($SERVICE_TYPE));
 	echo ("$contents");
 	?>

0495ffc6d23be9d2b9a428f35beaa25da2cf2a24
farhanrahim42 <farhan.rahim@tm.com.my>
2017-08-09 07:46:06 -0700

    Update index.php

--- JSONAccess/index.php
+++ JSONAccess/index.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+
         <?php
-        echo file_get_contents('http://itnthackathon.bweas.tm.com.my/api/getNodeLocation');
+        $IPMSAN=@$_GET['IPMSAN'];
+        $contents = file_get_contents('http://172.30.201.238:8080/api/getNodeLocation?transform=1&filter[]=IPMSAN,eq,'.$IPMSAN);
+        echo ("$contents");
         ?>
-    </body>
-</html>

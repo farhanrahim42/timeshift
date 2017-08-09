6998e0a28a82e244b80295fbd466f14cb85aa611
farhanrahim42 <farhan.rahim@tm.com.my>
2017-08-09 06:47:06 -0700

    Add files via upload

---
+++ JSONAccess5/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $IPMSAN=@$_GET['IPMSAN'];
+        $contents = file_get_contents('http://itnthackathon.bweas.tm.com.my/api/getNodeLocation?filter[]=IPMSAN,eq,'.$IPMSAN);
+        echo ("$contents");
+        ?>
+    </body>
+</html>

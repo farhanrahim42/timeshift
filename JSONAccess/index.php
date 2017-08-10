dde943edf654f6056279acb1a8a4c4c6354d3501
farhanrahim42 <farhan.rahim@tm.com.my>
2017-08-09 19:25:11 -0700

    Update index.php

--- JSONAccess/index.php
+++ JSONAccess/index.php
@@ -1,6 +1,6 @@
 
         <?php
-        $IPMSAN=@$_GET['IPMSAN'];
-        $contents = file_get_contents('http://172.30.201.238:8080/api/getNodeLocation?transform=1&filter[]=IPMSAN,eq,'.$IPMSAN);
+        $SERVICE_TYPE=@$_GET['SERVICE_TYPE'];
+        $contents = file_get_contents('http://172.30.201.238:8080/api/getNodeLocation?transform=1&filter[]=SERVICE_TYPE,eq,'.$SERVICE_TYPE);
         echo ("$contents");
         ?>

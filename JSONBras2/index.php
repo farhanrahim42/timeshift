26bb3e788e84f17edeeb24c37e146713579cbcbd
farhanrahim42 <farhan.rahim@tm.com.my>
2017-08-09 18:47:09 -0700

    Update index.php

--- JSONBras2/index.php
+++ JSONBras2/index.php
@@ -1,6 +1,6 @@
     <?php
-	$ACCESS_NAME=@$_GET['ACCESS_NAME'];
+	$TM_NODE=@$_GET['TM_NODE'];
 
-	$contents = file_get_contents('http://172.30.201.238:8080/api/getBRASInfo?transform=1&filter[]=ACCESS_NAME,eq,'.$ACCESS_NAME.'&filter[]=BRAS,eq,BSRJRC01');
+	$contents = file_get_contents('http://172.30.201.238:8080/api/getBRASInfo?transform=1&filter[]=TM_NODE,eq,'.$TM_NODE.'&filter[]=BRAS,eq,BSRJRC01');
 	echo ("$contents");
 	?>

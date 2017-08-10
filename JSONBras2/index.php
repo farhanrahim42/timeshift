ec27ec7b8dd528a1e71ba0b37b0e1f79bbb43509
farhanrahim42 <farhan.rahim@tm.com.my>
2017-08-09 17:42:11 -0700

    Update index.php

--- JSONBras2/index.php
+++ JSONBras2/index.php
@@ -1,4 +1,4 @@
     <?php
-	$contents = file_get_contents('http://itnthackathon.bweas.tm.com.my/api/getBRASInfo?transform=1&filter[]=ACCESS_NAME,eq,BNR_V1024&filter[]=BRAS,eq,BSRJRC01');
+	$contents = file_get_contents('http://172.30.201.238:8080/api/getBRASInfo?transform=1&filter[]=ACCESS_NAME,eq,BNR_V1024&filter[]=BRAS,eq,BSRJRC01');
 	echo ("$contents");
 	?>
